be51205416abeb1e6814d336aa2e3c52ebd06b4c
cksfect <ophcksfew@protonmail.com>
2025-05-23 00:24:35 -0700

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "cksfect/test:main"
+    [String]$upstream = "cksfect/test:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
